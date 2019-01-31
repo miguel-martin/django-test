4dceef54cc7ac277b3cf29241b31c230b1723657
Miguel <miguelm@unizar.es>
2019-01-31 08:43:31 +0100

    Más tests sobre Centros

--- envio/tests.py
+++ envio/tests.py
@@ -5,9 +5,9 @@ from .models import Centro, Estudio, Plan
 from django.urls import reverse
 
 
-def create_centro(codigo, nombre, url):
+def create_centro(cid, nombre, url, localidad):
 	""" Crea un centro """
-	return Centro.objects.create(cid=codigo, nombre=nombre, url=url)
+	return Centro.objects.create(cid=cid, nombre=nombre, localidad=localidad, url=url)
 
 
 class CentroModelTests(TestCase):
@@ -20,6 +20,32 @@ class CentroModelTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No hay centros en la base de datos.")
         self.assertQuerysetEqual(response.context['centros'], [])
+
+    def test_create_centro_and_view_listing(self):
+    	""" 
+    	Crea un centro y comprueba que aparece en el listado de centros
+    	"""
+    	create_centro(cid=110, nombre='Escuela de Ingeniería y Arquitectura', localidad='Z', url='http://eina.unizar.es')
+    	response = self.client.get(reverse('list_all_centros'))
+    	self.assertEqual(response.status_code, 200)
+    	self.assertContains(response, "Escuela de Ingeniería y Arquitectura")
+    	self.assertQuerysetEqual(
+    		response.context['centros'],
+    		['<Centro: Escuela de Ingeniería y Arquitectura>']
+    	)
+
+    def test_create_centro_and_view_details(self):
+    	""" 
+    	Crea un centro y comprueba que aparece el detalle del centro
+    	"""
+    	create_centro(cid=110, nombre='Escuela de Ingeniería y Arquitectura', localidad='Z', url='http://eina.unizar.es')
+    	response = self.client.get(reverse('list_centro', kwargs={'pk': 110}))
+    	self.assertEqual(response.status_code, 200)
+    	self.assertContains(response, "Escuela de Ingeniería y Arquitectura")
+    	self.assertQuerysetEqual(
+    		[response.context['centro']],
+    		['<Centro: Escuela de Ingeniería y Arquitectura>']
+    	)
 
 
 class EstudioModelTests(TestCase):
